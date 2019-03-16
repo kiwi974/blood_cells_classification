a476276d60fc278fff58160ed417f55924452530
kiwi974 <attachew974@gmail.com>
2019-03-16 10:36:50 +0100

    ok, still same prediction for any batch

--- TrainerOpt.py
+++ TrainerOpt.py
@@ -36,7 +36,7 @@ class TrainerOpt:
         self.next_element = next_element
         self.train_dataset = train_dataset
         self.test_dataset = test_dataset
-
+        self.current_labels = np.array([[0.,0.,0.,0.] for i in range(100)])
     
     def train(self, learning_rate, epochs, do_rate1, backup_folder):
         """
@@ -50,25 +50,29 @@ class TrainerOpt:
             - backu_folder : folder to which save the current trained model
         """
 
+        print(self.output.get_shape().ndims)
+        print(np.ndim(self.current_labels))
+        print(type(self.current_labels))
+
         # Make datasets that we can initialize separately, but using the same structure via the common iterator
         training_init_op = self.iterator.make_initializer(self.train_dataset)
         testing_init_op = self.iterator.make_initializer(self.test_dataset)
 
         # Define a loss function
         with tf.variable_scope('loss', reuse=tf.AUTO_REUSE):
-                loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = self.next_element[1], 
+            loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits_v2(labels = self.current_labels, 
                                                                                 logits = self.output))
         # Define an optimizer 
         with tf.variable_scope('train_op', reuse=tf.AUTO_REUSE):
-                train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+            train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
         # Convert logits to label indexes
         with tf.variable_scope('correct_pred', reuse=tf.AUTO_REUSE):
-                correct_pred = tf.argmax(self.output, axis=1)
+            correct_pred = tf.argmax(self.output, axis=1)
 
         # Equality between prediction and target prediciton 
         with tf.variable_scope('equality', reuse=tf.AUTO_REUSE):
-            equality = tf.equal(correct_pred, tf.argmax(self.next_element[1], 0))
+            equality = tf.equal(correct_pred, tf.argmax(self.current_labels,axis=1)) #tf.argmax(self.current_labels, 0))
 
         # Accuracy of the prediction 
         with tf.variable_scope('accuracy', reuse=tf.AUTO_REUSE):
@@ -98,23 +102,30 @@ class TrainerOpt:
 
                 for i in range(epochs):
                     sess.run(training_init_op)
-                    train_data = self.next_element[0]
-                    train_labels = self.next_element[1]
-
-                    _, loss_val, train_pred = sess.run([train_op, loss, correct_pred], feed_dict={self.input_placeholder: train_data.eval(), 
-                                                                            self.output_placeholder: train_labels.eval(),
-                                                                            self.should_drop : False,
-                                                                            self.dropout_rate1_placeholder : do_rate1})
+                    train_data = self.next_element[0].eval()
+                    self.current_labels = self.next_element[1].eval()
+                    
+                    _, loss_val = sess.run([train_op, loss], feed_dict={self.input_placeholder: train_data, 
+                                                                        self.output_placeholder: self.current_labels,
+                                                                        self.should_drop : False,
+                                                                        self.dropout_rate1_placeholder : do_rate1})
+                    print('\n%s\n' %(gradient))
                     losses.append(loss_val)
 
                     if (i%5 == 0):
                         accuracies_it.append(i)
 
                         # Accuracy on training set 
-                        train_acc = sess.run([accuracy], feed_dict={self.input_placeholder : train_data.eval(), 
-                                                                       self.should_drop : False,
-                                                                       self.dropout_rate1_placeholder : do_rate1})
-                        train_accuracies.append(train_acc[0])
+                        prediction, equ, train_acc = sess.run([correct_pred, equality, accuracy], feed_dict={self.input_placeholder : train_data, 
+                                                                    self.should_drop : False,
+                                                                    self.dropout_rate1_placeholder : do_rate1})
+                        print('\nprediction : ' + str(prediction))
+                        print('labels : ' + str(np.argmax(self.current_labels, axis=1)))
+                        equ = tf.equal(prediction, np.argmax(self.current_labels, axis=1)).eval()
+                        print('equality : ' + str(equ))#+ str(equ))
+                        acc_aux = tf.reduce_mean(tf.cast(equ, tf.float32)).eval()
+                        print('accuracy : ' + str(acc_aux))
+                        train_accuracies.append(train_acc) #[0])
 
                         # Accuracy on testing set 
                         sess.run(testing_init_op)
@@ -127,7 +138,7 @@ class TrainerOpt:
                         arrow_length = int(10*(i/epochs))
                         progress_percent = (int(1000*(i/epochs)))/10
                         sys.stdout.write('\r    \_ ITERATION : {1} / {2} ; loss = {3} ; training_acc = {4} ; testing_acc = {5} [{6}>{7}{8}%]'.format(
-                                                 1, i, epochs, '{0:.6f}'.format(loss_val), '{0:.6f}'.format(train_acc[0]), '{0:.6f}'.format(test_acc[0]),
+                                                 1, i, epochs, '{0:.6f}'.format(loss_val), '{0:.6f}'.format(train_acc), '{0:.6f}'.format(test_acc[0]),
                                                  '='*arrow_length,' '*(9-arrow_length), progress_percent))
                         sys.stdout.flush()
                 
@@ -136,19 +147,6 @@ class TrainerOpt:
                 print('\n\nModel saved in %s' % save_path)
                                 
 
-
-                ##### Testing #####
-
-                # Run predictions against the full test set.
-                #predicted = sess.run([correct_pred], feed_dict={self.input_placeholder: self.test_data, 
-                #                                                self.should_drop : False, 
-                #                                                 self.dropout_rate1_placeholder : do_rate1})[0]
-
-                # Calculate correct matches 
-                #match_count = sum([int(y == y_) for y, y_ in zip(self.test_labels, predicted)])
-
-                # Calculate the accuracy
-                #accuracy = match_count / len(self.test_labels)
 
                 writer.close()
 
